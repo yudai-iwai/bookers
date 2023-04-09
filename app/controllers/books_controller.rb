62bc7d2213dfa38226fd8b7c02f5728f4bdad1f2
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-09 14:11:56 +0000

    [add2]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,14 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
+  end
+  
   def index
   end
 
