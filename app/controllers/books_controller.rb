8bb88b9fc5db3ab3c674370502e0f69f02556958
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-10 04:23:24 +0000

    [add3]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,24 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
-    @book = Book.new(book_params)
+    book = Book.new(book_params)
     book.save
-    redirect_to '/show'
+    redirect_to '/books/show'
   end
   
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
