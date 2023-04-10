a8c8b6ca13f954b638db40973b561b3ab1fa6e00
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-10 14:07:54 +0000

    [add5]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/:id'
+    redirect_to '/books/show'
   end
   
   def index
