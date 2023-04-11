e487df148a43a7c51335d61f1f83c2f69a309d33
EC2 Default User <129721056+yudai-iwai@users.noreply.github.com>
2023-04-11 07:07:54 +0000

    [add9]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,13 +21,21 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
+   @books = Book.all
    book = Book.find(params[:id])
    book.destroy
    flash[:notice] = "Book was successfully destroyed!"
-   redirect_to books_path
+   render :index
+  end
+  
+  def update
+   book = Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book.id)
   end
 
   private
